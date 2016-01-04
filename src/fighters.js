df031a5ffab19a0eaeab64b0862ec382b7892a15
suvrajyotichatterjee <alfapagal@gmail.com>
2016-01-04 07:34:42 +0530

    feat(Data): Added new fighters

--- src/fighters.js
+++ src/fighters.js
@@ -3,7 +3,15 @@ var Fighter = function(name, wtClass){
     this.name = name;
     this.class = wtClass;
 };
+result.push(new Fighter('Ronda Rousey', 2));
+result.push(new Fighter('Holly Holm', 2));
+result.push(new Fighter('Miesha Tate', 2));
 result.push(new Fighter('Connor McGregor', 3));
+result.push(new Fighter('Chad Mendes', 3));
+result.push(new Fighter('Jose Aldos', 3));
+result.push(new Fighter('Rafael Dos Anjos', 4));
+result.push(new Fighter('Nate Diaz', 4));
 result.push(new Fighter('Anderson Silva', 6));
+result.push(new Fighter('Nick Diaz', 6));
 result.push(new Fighter('Jon Jones', 7));
 module.exports= result;
